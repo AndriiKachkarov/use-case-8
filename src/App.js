4f5aa934949de8c4bec21d864aa70add221e4cd9
akachk <akachk@softserveinc.com>
2023-08-09 12:01:31 +0300

    UC#8 Setup the form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import './App.css';
+import {useDispatch, useSelector} from 'react-redux';
+import {useState} from 'react' ;
+import validator from 'validator';
+import {selectUserData, setUserData} from './redux';
+
+const MIN_MESSAGE_LENGTH = 10;
 
 function App() {
+  const userData = useSelector(selectUserData);
+
+  const [formData, setFormData] = useState(userData);
+  const dispatch = useDispatch();
+
+  const {firstName, lastName, email, message} = formData;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(setUserData(formData));
+    console.log(`${firstName} ${lastName} <${email}> says: "${message}"`)
+  };
+
+  const handleChange = e => setFormData(prevState => ({
+    ...prevState,
+    [e.target.name]: e.target.value,
+  }));
+
+  const isFirstNameValid = !validator.isEmpty(firstName, {ignore_whitespace: true});
+  const isLastNameValid = !validator.isEmpty(lastName, {ignore_whitespace: true});
+  const isEmailValid = validator.isEmail(email);
+  const isMessageValid = validator.isLength(message, {min: MIN_MESSAGE_LENGTH});
+
+  const isFormDisabled = !(isFirstNameValid && isLastNameValid && isEmailValid && isMessageValid)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <form onSubmit={handleSubmit}>
+          <label>
+            First name:
+            <input name="firstName" type="text" value={firstName} onChange={handleChange}/>
+          </label>
+          <label>
+            Last name:
+            <input name="lastName" type="text" value={lastName} onChange={handleChange}/>
+          </label>
+          <label>
+            Email:
+            <input name="email" type="email" value={email} onChange={handleChange}/>
+          </label>
+          <label>
+            message:
+            <input name="message" type="text" value={message} onChange={handleChange}/>
+          </label>
+          <button type="submit" disabled={isFormDisabled}>Submit</button>
+        </form>
+      </div>
+  )
+      ;
 }
 
 export default App;
